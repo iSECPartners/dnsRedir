983182eccae90ad33ad5813d264edf7048154b5e
Tim Newsham <tim.newsham@gmail.com>
2013-06-28 09:12:43 -1000

    - walk through to cleanup stuff

--- dnsRedir.py
+++ dnsRedir.py
@@ -9,28 +9,33 @@ NOTES:
   - no attempt is made to make IDs unguessable.  This is a security
     weakness that can be exploited in a hostile enviornment.
   - will complain about slack data
+
 TODO:
-  - more record types: AAAA, PTR, TXT, ...
+  - more record types if needed: AAAA, PTR, TXT, ...
 """
 
 import optparse, re, socket, struct, time
 
 publicDNS = '8.8.8.8' # google's public DNS server
 publicDNS6 = '::ffff:' + publicDNS
+gQuiet = False
 
 QUERY,IQUERY = 0,1
 IN = 1
 A,NS,CNAME,PTR,MX,TXT = 1,2,5,12,15,16
+LABLEN,LABOFF = 0,3
 
 class Error(Exception) :
     pass
 
 def log(fmt, *args) :
-    now = time.time()
-    ts = time.strftime('%Y-%m-%d:%H:%M:%S', time.localtime(now))
-    print ts, fmt % args
+    if not gQuiet :
+        now = time.time()
+        ts = time.strftime('%Y-%m-%d:%H:%M:%S', time.localtime(now))
+        print ts, fmt % args
 
 def getBits(num, *szs) :
+    """Get bits from num from right to left."""
     rs = []
     for sz in szs :
         mask = (1 << sz) - 1
@@ -38,6 +43,7 @@ def getBits(num, *szs) :
         num >>= sz
     return rs
 def putBits(szs, *args) :
+    """Put bits into an integer from right to left."""
     num = 0
     sh = 0
     for (sz,a) in zip(szs, args) :
@@ -46,42 +52,44 @@ def putBits(szs, *args) :
         sh += sz
     return num
 
-def unpack(fmt, buf, off) :
+def getPacked(fmt, buf, off) :
     sz = struct.calcsize(fmt)
     return struct.unpack(fmt, buf[off : off + sz]), off+sz
-def pack(buf, fmt, *args) :
+def putPacked(buf, fmt, *args) :
     buf.append(struct.pack(fmt, *args))
 
 def getLabel(buf, off, ctx) :
     """Get a DNS label, without any decompression."""
-    (b,),dummy = unpack('!B', buf, off)
-    t = b >> 6
-    if t == 0 : # len
-        v = (t, buf[off + 1 : off + 1 + b], off)
-        off2 = off + 1 + b
-    elif t == 3 : # off
-        (x,),off2 = unpack('!H', buf, off)
-        x &= 0x3fff
-        v = (t, x, off)
+    (b,),dummy = getPacked('!B', buf, off)
+    typ = b >> 6
+    if typ == LABLEN : 
+        endOff = off + 1 + b
+        name = buf[off + 1 : endOff]
+        val = name
+    elif typ == LABOFF :
+        (ptr,),endOff = getPacked('!H', buf, off)
+        ptr &= 0x3fff
+        val = ptr
     else :
-        raise Error("invalid label type %d at %d" % (t, off))
-    return v,off2
+        raise Error("invalid label type %d at %d" % (typ, off))
+    return (typ, val, off), endOff
 
 def getDomName(buf, off, ctx) :
     """Get a domain name, performing decompression."""
     idx = off
     labs = []
+    endOff = off
     while True :
-        l,off = getLabel(buf, off, ctx)
-        labs.append(l)
-        if l[0] != 0 or l[1] == '' : # terminate at empty label or pointer
+        (t,v,o),endOff = getLabel(buf, endOff, ctx)
+        labs.append((t,v,o))
+        if t == LABOFF or v == '' : # terminate at pointer or empty name
             break
 
     if idx not in ctx : # decompress
-        ctx[idx] = None
+        ctx[idx] = None # Avoid loops during recursion. see below.
         r = []
         for t,v,o in labs :
-            if t == 0 :
+            if t == LABLEN :
                 r.append(v)
             else :
                 name,dummy = getDomName(buf, v, ctx)
@@ -89,28 +97,28 @@ def getDomName(buf, off, ctx) :
         ctx[idx] = '.'.join(r)
     if ctx[idx] is None :
         raise Error("invalid loop in domain decompression at %d" % o)
-    return ctx[idx],off
+    return ctx[idx],endOff
 
 def putDomain(buf, dom) :
     """Put a domain name. Never compressed..."""
     labs = dom.rstrip('.').split('.')
     if len(dom) > 255 or any(len(l) > 63 or len(l) == 0 for l in labs) :
         raise Error("Cannot encode domain: %s" % dom)
-    labs.append('')
+    labs.append('') # terminator
     for l in labs :
-        pack(buf, "!B", len(l))
+        putPacked(buf, "!B", len(l))
         buf.append(l)
 
 class DNSQuestion(object) :
     def get(self, buf, off, ctx) :
         self.name,off = getDomName(buf, off, ctx)
-        (self.type,self.klass),off = unpack("!HH", buf, off)
+        (self.type,self.klass),off = getPacked("!HH", buf, off)
         return off
     def put(self, buf) :
         putDomain(buf, self.name)
-        pack(buf, "!HH", self.type, self.klass)
+        putPacked(buf, "!HH", self.type, self.klass)
     def __str__(self) :
-        return '[Quest %s %s %s]' % (self.name, self.type, self.klass)
+        return '[Q %s %s %s]' % (self.name, self.type, self.klass)
 
 class DNSResA(object) :
     def __init__(self, val=None) :
@@ -135,7 +143,7 @@ class DNSResRec(object) :
     }
     def get(self, buf, off, ctx) :
         self.name,off = getDomName(buf, off, ctx)
-        (self.type,self.klass,self.ttl, l),off = unpack("!HHIH", buf, off)
+        (self.type,self.klass,self.ttl, l),off = getPacked("!HHIH", buf, off)
         self.nested = buf[off : off + l] 
         off += l 
 
@@ -155,21 +163,21 @@ class DNSResRec(object) :
 
         putDomain(buf, self.name)
         l = len(self.nested)
-        pack(buf, "!HHIH", self.type, self.klass, self.ttl, l)
+        putPacked(buf, "!HHIH", self.type, self.klass, self.ttl, l)
         buf.append(self.nested)
 
     def __str__(self) :
+        v = repr(self.nested)
         if self.val :
-            return '[RR %s %s %s %s %s]' % (self.type, self.klass, self.ttl, self.name, self.val)
-        else :
-            return '[RR %s %s %s %s %r]' % (self.type, self.klass, self.ttl, self.name, self.nested)
+            v = self.val
+        return '[RR %s %s %s %s %s]' % (self.type, self.klass, self.ttl, self.name, v)
 
 def getArray(buf, off, cnt, constr, ctx) :
     objs = []
     for n in xrange(cnt) :
         obj = constr()
-        off = obj.get(buf, off, ctx)
         objs.append(obj)
+        off = obj.get(buf, off, ctx)
     return objs, off
 def putArray(buf, arr) :
     for obj in arr :
@@ -185,21 +193,22 @@ class DNSMsg(object) :
 
         if buf is not None :
             self.get(buf)
+
     def get(self, buf) :
         ctx = {}
-        self.id, bits, qdcount, ancount, nscount, arcount = struct.unpack("!HHHHHH", buf[:12])
+        (self.id, bits, qdcount, ancount, nscount, arcount),n = getPacked("!HHHHHH", buf, 0)
         self.rcode, self.z, self.ra, self.rd, self.tc, self.aa, self.opcode, self.qr = getBits(bits, 4, 3, 1, 1, 1, 1, 4, 1)
-        n = 12
         self.qd,n = getArray(buf, n, qdcount, DNSQuestion, ctx)
         self.an,n = getArray(buf, n, ancount, DNSResRec, ctx)
         self.ns,n = getArray(buf, n, nscount, DNSResRec, ctx)
         self.ar,n = getArray(buf, n, arcount, DNSResRec, ctx)
         if n < len(buf) :
             raise Error("unexpected slack data: %r" % buf[n:])
+
     def put(self) :
         buf = []
         bits = putBits((4, 3, 1, 1, 1, 1, 4, 1), self.rcode, self.z, self.ra, self.rd, self.tc, self.aa, self.opcode, self.qr)
-        pack(buf, "!HHHHHH", self.id, bits, len(self.qd), len(self.an), len(self.ns), len(self.ar))
+        putPacked(buf, "!HHHHHH", self.id, bits, len(self.qd), len(self.an), len(self.ns), len(self.ar))
         putArray(buf, self.qd)
         putArray(buf, self.an)
         putArray(buf, self.ns)
@@ -213,13 +222,12 @@ class DNSMsg(object) :
         arrs = 'qd=%s an=%s ns=%s ar=%s' % tuple(arrStr(x) for x in (self.qd,self.an,self.ns,self.ar))
         return '[DNSMsg id=%d rcode=%d z=%d ra=%d rd=%d tc=%d aa=%d opcode=%d qr=%d %s]' % (self.id, self.rcode, self.z, self.ra, self.rd, self.tc, self.aa, self.opcode, self.qr, arrs)
 
-def findMatch(opt, ty, name) :
-    for ty_,pat,val in opt.names :
-        print pat, name, re.match(pat, name)
+def lookup(db, ty, name) :
+    for ty_,pat,val in db :
         if ty == ty_ and re.match(pat, name) :
             return val
 
-def answer(q, val, ttl, id, opcode) :
+def mkResp(q, val, ttl, id, opcode) :
     a = DNSResRec()
     a.name, a.type, a.klass = q.name, q.type, q.klass
     a.ttl, a.val = ttl, val
@@ -237,10 +245,10 @@ def procQuery(opt, s, m, peer) :
     if m.opcode == QUERY and len(m.qd) == 1 :
         q = m.qd[0]
         if q.klass == IN and q.type == A :
-            ip = findMatch(opt, 'A', q.name)
+            ip = lookup(opt.names, 'A', q.name)
             if ip :
                 log("Answering %s/%d query IN A %s with %s", peer, m.id, q.name, ip)
-                resp = answer(q, DNSResA(ip), opt.ttl, m.id, m.opcode)
+                resp = mkResp(q, DNSResA(ip), opt.ttl, m.id, m.opcode)
     return resp
 
 class Proxy(object) :
@@ -248,7 +256,18 @@ class Proxy(object) :
     timeo = 30
     id = 1
     tab = {}
+
+    @staticmethod
+    def clean() :
+        self = Proxy
+        now = time.time()
+        for k,v in self.tab.items() :
+            if v.expire <= now :
+                log("expire proxy request %d", k)
+                del self.tab[k]
+
     def __init__(self, peer, msg) :
+        """Make a proxy object and put it in the prox table."""
         self.expire = time.time() + self.timeo
         self.peer = peer
         self.origId = msg.id
@@ -261,21 +280,13 @@ class Proxy(object) :
 
         Proxy.id = (Proxy.id + 1) & 0xffff # weak ID generation
 
-    @staticmethod
-    def clean() :
-        self = Proxy
-        now = time.time()
-        for k,v in self.tab.items() :
-            if v.expire <= now :
-                log("expire proxy request %d", k)
-                del self.tab[k]
 
 def sendMsg(s, addr, msg) :
     buf = msg.put()
     if s.sendto(buf, addr) != len(buf) :
         raise Error("failure sending msg: " + e)
 
-def procMsg(opt, s, buf, peer) :
+def procMsg(opt, sock, buf, peer) :
     Proxy.clean()
 
     m = DNSMsg()
@@ -284,24 +295,24 @@ def procMsg(opt, s, buf, peer) :
     except Error, e :
         log("Error parsing msg from %s: %s", peer, e)
         return
-    resp = None
+
     if m.qr == 0 : # query from client - answer it or proxy it
-        resp = procQuery(opt, s, m, peer)
+        resp = procQuery(opt, sock, m, peer)
         if resp is not None : 
             log("Send answer to %s", peer)
-            sendMsg(s, peer, resp)
+            sendMsg(sock, peer, resp)
         else : # not processed, proxy it
             p = Proxy(peer, m)
             log("Proxy msg from client %s/%d to server %s/%d", peer, m.id, opt.srv, p.id)
             m.id = p.id
-            sendMsg(s, opt.srv, m)
+            sendMsg(sock, opt.srv, m)
     else : # response from server - proxy back to client
         p = Proxy.tab.get(m.id)
         if p is not None :
             del Proxy.tab[m.id]
             log("Proxy msg from server %s/%d to client %s/%d", peer, m.id, p.peer, p.origId)
             m.id = p.origId
-            sendMsg(s, p.peer, m)
+            sendMsg(sock, p.peer, m)
         else : 
             log("Unexpected response from %s/%d", peer, m.id)
 
@@ -317,7 +328,7 @@ def server(opt) :
         except Exception, e :
             pass
     s.bind((opt.bindAddr, opt.port))
-    while 1 :
+    while True :
         buf,peer = s.recvfrom(64 * 1024)
         log("Received %d bytes from %s", len(buf), peer)
         try :
@@ -326,18 +337,19 @@ def server(opt) :
             log("Error processing from %s", peer)
 
 def mkIP(xs) :
+    assert len(xs) == 4
     return '.'.join('%d' % ord(x) for x in xs)
 def parseIP(xs) :
     try :
         a,b,c,d = map(int, xs.split('.'))
         if any(x < 0 or x > 255 for x in (a,b,c,d)) :
-            raise Error("bad ip")
+            raise Error("jump to handler below")
         return ''.join(chr(x) for x in (a,b,c,d))
     except :
         raise Error("Bad IP address format: %r" % xs)
 
 def parseNames(args) :
-    map = []
+    tab = []
     for a in args :
         ws = a.split(":")
         if len(ws) != 3 :
@@ -348,8 +360,8 @@ def parseNames(args) :
             dummy = parseIP(val)
         else :
             raise Error("Unsupported query type %r in %r" % (ty, a))
-        map.append((ty,nm,val))
-    return map
+        tab.append((ty,nm,val))
+    return tab
 
 def getopts() :
     p = optparse.OptionParser(usage="usage: %prog [opts] [type:name:val ...]")
@@ -358,12 +370,15 @@ def getopts() :
     p.add_option('-p', dest='port', type=int, default=53, help='Port to listen on. Default=53')
     p.add_option('-P', dest='dnsServerPort', type=int, default=53, help='Port of default DNS server. Default=53')
     p.add_option('-t', dest='ttl', type=int, default=30, help='TTL for responses. Default=30 seconds')
+    p.add_option('-q', dest='quiet', action='store_true', help='Quiet')
     p.add_option('-6', dest='six', action='store_true', help='Use IPv6 server socket')
     opt,args = p.parse_args()
     opt.names = parseNames(args)
     if opt.dnsServer == None :
         opt.dnsServer = publicDNS6 if opt.six else publicDNS
     opt.srv = opt.dnsServer, opt.dnsServerPort
+    global gQuiet
+    quiet = opt.quiet
     return opt
 
 def main() :
